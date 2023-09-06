58703ba0840c068efa42c3f1b06311f41acf1649
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-09-06 20:05:27 -0300

    Clase API

---
+++ Clase 5/clase5.js
@@ -0,0 +1,24 @@
+/* 
+Cuando quiero PEDIR INFORMACIÓN el método se llama GET 
+Cuando quiero ENVIAR información se envía con POST. Va a "viajar" mi pedido por POST 
+Cuando quiero ACTUALIZAR algo que ya está en la base de datos PUT
+Cuando quiero ELIMINAR lo hago por DELETE
+
+MIDDELWARES: Bloques de seguridad.
+Cuando me quiero loggear y no estoy registrado, estoy haciendo - como cliente-
+ un pedido. No hace falta que llegue al servido y se fije si estoy en la base de dato. 
+Puedo tener un bloque en el medio que compare si dicha persona está registrado
+
+REQUEST - MIDDELWARES - REPORT
+
+API . Interfaz de programacion de aplicaciones.
+Pieza de código que permite a diferentes aplicaciones comunicarse entre si y compartir 
+información. Es como un intérprete.
+Es lo que permite que "hablemos" el mismo idioma entre el cliente y el servidor. El 
+middelware es como la "carta" que le muestra al mozo el contenido de lo que hay o no hay.
+
+
+*/
+
+
+console.log("Hola mundo");
