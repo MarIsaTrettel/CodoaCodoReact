43c124e3dc1e461a8c6f5517e1e311e4b8178084
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-09-18 22:41:47 -0300

    Clase 10

---
+++ Clase10/async-await.js
@@ -0,0 +1,12 @@
+let getNameAsync = async (idPost) => {
+    try {
+        let resPost = await fetch (`https://jsonplaceholder.typicode.com/posts/${idPost}`)
+        let post = await resPost.json();
+        console.log(post.title);
+
+    }catch (error) {
+        console.log(error);
+    }
+}
+
+getNameAsync(14)
