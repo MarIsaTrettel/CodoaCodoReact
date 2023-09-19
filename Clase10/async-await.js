a4fd1c108d998d8ec8a1037aec210a11a187a003
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-09-19 19:58:30 -0300

    clase

--- Clase10/async-await.js
+++ Clase10/async-await.js
@@ -2,8 +2,11 @@ let getNameAsync = async (idPost) => {
     try {
         let resPost = await fetch (`https://jsonplaceholder.typicode.com/posts/${idPost}`)
         let post = await resPost.json();
-        console.log(post.title);
+        
+        let resUser = await fetch (`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+        let user = await resUser.json()
 
+        document.write (`${user.name} escribió el post ${idPost} y vive en ${user.address.city}`)
     }catch (error) {
         console.log(error);
     }
