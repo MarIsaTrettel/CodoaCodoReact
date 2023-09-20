07089b7aabfff17a4e2694ed321f7b8164467b94
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-09-19 21:07:14 -0300

    clase

--- Clase10/axios.js
+++ Clase10/axios.js
@@ -1,6 +1,9 @@
 let getNameAxios = async (idPost) =>{
     try {
-        let resPost = await axios
+        let resPost = await axios (`https://jsonplaceholder.typicode.com/posts/${idPost}`)
+        console.log(resPost.data.title);
+
+        let resUser = await axios (`https://jsonplaceholder.typicode.com/users/${post.userId}`)
     } catch (error) {
         console.log(error);
     }
