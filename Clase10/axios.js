1489460718d47fd85820dc0d65dab18cd56d9c9b
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-09-19 21:26:44 -0300

    clase

--- Clase10/axios.js
+++ Clase10/axios.js
@@ -3,7 +3,9 @@ let getNameAxios = async (idPost) =>{
         let resPost = await axios (`https://jsonplaceholder.typicode.com/posts/${idPost}`)
         console.log(resPost.data.title);
 
-        let resUser = await axios (`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+        let resUser = await axios (`https://jsonplaceholder.typicode.com/users/${resPost.data.userId}`)
+        console.log(resUser.data);
+        document.write(`${resUser.data.name} escribió el post ${resPost.data.title} vive en ${resUser.data.address.city}`);
     } catch (error) {
         console.log(error);
     }
