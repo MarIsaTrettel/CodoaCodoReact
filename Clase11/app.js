2d91ec665f0f06d80f50d01aa07d4c11696819d6
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-09-22 20:04:53 -0300

    clase11

--- Clase11/app.js
+++ Clase11/app.js
@@ -39,9 +39,19 @@ let btnSiguiente = document.querySelector(".btnSiguiente");
     try {
         let respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=ef0c38873ac480926387f7960b82f6a0&language=es-MX&page=${pagina}`);
         console.log(respuesta);
-        if (respuesta.status == 200){
+        if (respuesta.status == 200){ //https://developer.mozilla.org/en-US/docs/Web/API/Response/json
             let datos = await respuesta.json()
-            console.log(datos);
+            console.log(datos.results);
+            let peliculas = "";
+
+            datos.results.foreach((pelicula =>{
+                peliculas += `<div class="peliculas">
+                <img class="poster" src="http://image.tmdb.org/t/p/w500${pelicula.poster_path}"/>
+                <h3>${pelicula.title}</h3>
+                </div>`
+            })
+
+            )
         }
     } catch (error) {
         
