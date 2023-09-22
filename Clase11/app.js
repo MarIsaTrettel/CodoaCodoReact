c0f12e2717da823d03a0280aec9c860250ed3f6e
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-09-22 19:20:28 -0300

    clase11

--- Clase11/app.js
+++ Clase11/app.js
@@ -1,3 +1,7 @@
+window.addEventListener("load", () =>{
+    cargarPeliculas();
+})
+
 let pagina = 1;
 
 let btnAnterior = document.querySelector(".btnAnterior");
@@ -9,8 +13,10 @@ let btnSiguiente = document.querySelector(".btnSiguiente");
     if (pagina > 1){
         pagina -= 1;
         // cargar la página
+        cargarPeliculas();
     }else{
         pagina = 500;
+        cargarPeliculas();
     }
  })
 
@@ -20,8 +26,10 @@ let btnSiguiente = document.querySelector(".btnSiguiente");
     if (pagina < 500 ){
         pagina += 1;
         // cargar la página
+        cargarPeliculas();
     } else {
         pagina = 1;
+        cargarPeliculas();
     }
  })
 
@@ -29,8 +37,10 @@ let btnSiguiente = document.querySelector(".btnSiguiente");
 
  const cargarPeliculas = async () =>{
     try {
-        
+        let respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=ef0c38873ac480926387f7960b82f6a0&language=es-MX&page=${pagina}`);
+        console.log(respuesta);
     } catch (error) {
         
     }
  }
+ cargarPeliculas();
