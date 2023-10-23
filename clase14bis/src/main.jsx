cf8a776015603ba4eb0bd2d7680bc071380e332b
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-10-23 15:23:15 -0300

    clase 14 bis

--- clase14bis/src/main.jsx
+++ clase14bis/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Boton from './Boton.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Boton />
   </React.StrictMode>,
 )
