63ffff689309576097278e638583a63f52edfbb6
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-11-01 21:14:54 -0300

    Clase 16 bis

--- clase16bis/clase16bite/src/Counter.jsx
+++ clase16bis/clase16bite/src/Counter.jsx
@@ -1,21 +1,24 @@
-
+import { useState } from "react"
 import { Boton } from "./Boton"
 
 export const Counter = () => {
-    return(
+    return (
         <h1>COUNTER new</h1>
 
     )
 }
 
 export const Counter2 = () => {
-    return(
+
+    const [contador, setContador] = useState (0)
+
+    return (
         <>
-        <h1>COUNTER 2</h1>
-        <Boton numero={1} onClick={()=>alert("Presionaste el boton")}/>
-        <Boton numero={2}/>
+            <h1>COUNTER 2</h1>
+            <Boton numero={1} onClick={() => alert("Presionaste el boton")} />
+            <Boton numero={2} />
         </>
-        
+
 
     )
 }
