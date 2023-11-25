96912fc9e93b66795949e026f1c87d8df0507fe4
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-11-24 22:17:26 -0300

    Trabajo practico

---
+++ clase18/src/App.jsx
@@ -0,0 +1,15 @@
+import { DrinksGrid } from "./DrinksGrid"
+
+
+
+export const App = () => {
+
+    return (
+        <div>
+            <header className="title"></header>
+            <main>
+                <DrinksGrid />
+            </main>
+        </div>
+    )
+};
---
+++ clase18/src/DrinksCard.jsx
@@ -0,0 +1,14 @@
+import "./DrinksCard.css";
+
+export const DrinksCard = (drink) => {
+    
+    return (
+        <li className="drinksCard">
+            <img className="drinkImage" src={drink.drink.strDrinkThumb} alt={drink.drink.strDrink} />
+            <div>
+                {drink.drink.strDrink}
+            </div>
+        </li>
+        
+    )
+}
