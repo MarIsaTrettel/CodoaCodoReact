c9389004c2636f66a52d2b5c14f734a076ddbeaa
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-11-25 01:09:12 -0300

    Trabajo practico

--- clase17/src/components/DrinksCard.jsx
+++ clase17/src/components/DrinksCard.jsx
@@ -1,4 +1,6 @@
+import {Link} from "react-router-dom";
 import "./DrinksCard.css";
+
 
 
 export const DrinksCard = (drink) => {
@@ -6,12 +8,12 @@ export const DrinksCard = (drink) => {
     return (
     
         <li className="drinksCard">
-            
+            <Link to={`/lookup.php?i=${drink.drink.idDrink}`} className="drinksCardLink" >
             <img className="drinkImage" src={drink.drink.strDrinkThumb} alt={drink.drink.strDrink} />
-
-            <div>
+            <div className="drinksName">
                 {drink.drink.strDrink}
             </div>
+            </Link>
         </li>
         
     )
