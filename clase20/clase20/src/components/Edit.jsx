7db7ef95d1c05df383f219b07e5ebb5ed7acae64
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-12-08 20:30:34 -0300

    clase 24

--- clase20/clase20/src/components/Edit.jsx
+++ clase20/clase20/src/components/Edit.jsx
@@ -1,54 +1,60 @@
 import { useState, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
-import { getDoc, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { getDoc, doc, updateDoc } from "firebase/firestore";
 import { db } from "../firebaseConfig/firebase.js";
 
 
 export const Edit = () => {
 
-const [name, setName] = useState("")
-const [lastname, setLastname] = useState("")
-const [age, setAge] = useState("")
-const [power, setPower] = useState("")
+    const [name, setName] = useState("")
+    const [lastname, setLastname] = useState("")
+    const [age, setAge] = useState("")
+    const [power, setPower] = useState("")
 
-const navigate = useNavigate();
+    const navigate = useNavigate();
 
-const {id} = useParams();
+    const { id } = useParams();
 
-const update = async(e) => {
-    e.preventDefault();
-    const heroeDoc = doc (db, "heroes", id);
+    const update = async (e) => {
+        e.preventDefault();
+        const heroeDoc = doc(db, "heroes", id);
 
-    const data = {
-        name: name,
-        lastname: lastname,
-        age: age,
-        power: power
+        const data = {
+            name: name,
+            lastname: lastname,
+            age: age,
+            power: power,
+        };
+
+        await updateDoc(heroeDoc, data);
+        navigate("/"); //en la de tragos poner /show
     };
 
-    await updateDoc (heroeDoc, data)
-    navigate ("/") //en la de tragos poner /show
+        const getHeroesById = async (id) => {
+            const heroeDoc = await getDoc(doc(db, "heroes", id));
 
-    const getHeroesById = async (id) =>{
-        const heroeDoc = await getDoc (doc(db, "heroes", id));
+            if (heroeDoc.exists()) {
+                setName(heroeDoc.data().name);
+                setLastname(heroeDoc.data().lastname);
+                setAge(heroeDoc.data().age);
+                setPower(heroeDoc.data().power);
+            } else {
+                console.log("No existe el dato");
+            }
+        };
 
-        if (heroeDoc.exists()){
-            setName(heroeDoc.data().name);
-            setLastname(heroeDoc.data().lastname);
-            setAge(heroeDoc.data().age);
-            setPower(heroeDoc.data().power);
-        }else{
-            alert("No existe el dato");
-        }
-    }
-};
+        // Use Effect
+        useEffect(() => {
+            getHeroesById(id);
+        }, []);
 
-    // Use Effect
-    useEffect(() => {
-        getHeroesById(id);
-    }, []);
-
-    return (
-        <h1>Aca se van a editar los documentos</h1>
-    )
-}
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h1>Edit Heroe</h1>
+                    </div>
+                </div>
+            </div>
+        )
+    };
