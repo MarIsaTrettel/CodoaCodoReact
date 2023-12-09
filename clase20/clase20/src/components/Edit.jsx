ab4a79032224a84ecd221a4c7bf7c3dc0253f6a3
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-12-08 22:36:31 -0300

    clase 24

--- clase20/clase20/src/components/Edit.jsx
+++ clase20/clase20/src/components/Edit.jsx
@@ -30,31 +30,55 @@ export const Edit = () => {
         navigate("/"); //en la de tragos poner /show
     };
 
-        const getHeroesById = async (id) => {
-            const heroeDoc = await getDoc(doc(db, "heroes", id));
+    const getHeroesById = async (id) => {
+        const heroeDoc = await getDoc(doc(db, "heroes", id));
 
-            if (heroeDoc.exists()) {
-                setName(heroeDoc.data().name);
-                setLastname(heroeDoc.data().lastname);
-                setAge(heroeDoc.data().age);
-                setPower(heroeDoc.data().power);
-            } else {
-                console.log("No existe el dato");
-            }
-        };
+        if (heroeDoc.exists()) {
+            setName(heroeDoc.data().name);
+            setLastname(heroeDoc.data().lastname);
+            setAge(heroeDoc.data().age);
+            setPower(heroeDoc.data().power);
+        } else {
+            console.log("No existe el dato");
+        }
+    };
 
-        // Use Effect
-        useEffect(() => {
-            getHeroesById(id);
-        }, []);
+    // Use Effect
+    useEffect(() => {
+        getHeroesById(id);
+    }, []);
 
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <h1>Edit Heroe</h1>
-                    </div>
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1>Edit Heroe</h1>
+                    <form onSubmit={update}>
+                        <div className="mb-3">
+                            <label className="form-label">NOMBRE</label>
+                            <input className="form-control" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                        </div>
+
+                        <div className="mb-3">
+                            <label className="form-label">APELLIDO</label>
+                            <input className="form-control" type="text" value={lastname} onChange={(e) => setLastname(e.target.value)} />
+                        </div>
+
+                        <div className="mb-3">
+                            <label className="form-label">EDAD</label>
+                            <input className="form-control" type="text" value={age} onChange={(e) => setAge(e.target.value)} />
+                        </div>
+
+                        <div className="mb-3">
+                            <label className="form-label">PODER</label>
+                            <input className="form-control" type="text" value={power} onChange={(e) => setPower(e.target.value)} />
+                        </div>
+
+                        <button type="submit" className="btn btn-secondary">EDITAR</button>
+                    </form>
                 </div>
             </div>
-        )
-    };
+        </div>
+
+    )
+};
